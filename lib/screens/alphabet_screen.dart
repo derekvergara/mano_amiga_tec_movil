013fae76f6ade72b9ee57817e2bdc4c90c173fc5
Derek Vergara <dereckvergara12@gmail.com>
2025-01-31 11:49:27 -0500

    .

--- lib/screens/alphabet_screen.dart
+++ lib/screens/alphabet_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'settings_screen.dart';
 import 'home_screen.dart';
 
-// derek es meco
 class AlphabetScreen extends StatelessWidget {
   final String title;
 
